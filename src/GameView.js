73dffafaf423ff60f065c6a264b010860ec0a541
Lisandro Mayancela <lmayancela08@gmail.com>
2018-03-05 23:28:27 +0000

    component made

---
+++ src/GameView.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import WikiApi from './WikiApi';
+
+export default class GameView extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            summary: "Loading..."
+        };
+    }
+    componentDidMount() {
+        var self = this
+        WikiApi.getSummary(this.props.title).then(
+            function(text){
+                self.setState({
+                    summary: text
+                });
+            });
+    }
+    
+    render() {
+        var currentTitle = this.props.title;
+
+        return (
+            <div>
+                {this.state.summary}
+            </div>
+        );
+    }    
+}
